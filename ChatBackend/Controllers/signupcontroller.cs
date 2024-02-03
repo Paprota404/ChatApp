aced0aa6821d48e7771df78720bcdc64489673d6
Paprota <paprota404@gmail.com>
2024-02-03 21:35:54 +0100

    Connecting DB

--- ChatBackend/Controllers/signupcontroller.cs
+++ ChatBackend/Controllers/signupcontroller.cs
@@ -6,13 +6,18 @@ namespace SignUp.Controllers
 {
     [HttpPost]
     public class SignUpController : ApiController{
-        public IHttpActionResult SignUp(SignUpModel model){
+        public async IHttpActionResult SignUp(SignUpModel model){
             if(!ModelState.IsValid){
                 return BadRequest(ModelState);
             }
             
 
             model.Password = BCrypt.Net.BCrypt.HashPassword(model.Password);
+
+            var user = new SignUpModel { Email = model.Email, Password = model.Password }; 
+            _context.Users.Add(user); 
+            await _context.SaveChangesAsync();
+
         }
     }
 }
--- ChatBackend/DataAccess/DbContext.cs
+++ ChatBackend/DataAccess/DbContext.cs
@@ -1,6 +1,6 @@
 public class AppDbContext : DbContext{
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){
-
+optionsBuilder.UseNpgsql(Configuration.GetConnectionString("PgDbConnection"));
     }
 
     public DbSet<SignUpModel> Users {get; set;}
