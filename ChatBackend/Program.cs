728bdca44c0a9f6db307d660d92277caa7fc9dc2
Paprota <paprota404@gmail.com>
2024-02-04 21:27:53 +0100

    GIT

--- ChatBackend/Program.cs
+++ ChatBackend/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("YourDbContext")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext")));
 
 var app = builder.Build();
 
