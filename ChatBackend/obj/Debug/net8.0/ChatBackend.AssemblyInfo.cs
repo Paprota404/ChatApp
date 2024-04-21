c06f0ed7c579a90531fe7970f897274b358e80fa
Paprota <paprota404@gmail.com>
2024-04-21 22:49:17 +0200

    SQL

--- ChatBackend/obj/Debug/net8.0/ChatBackend.AssemblyInfo.cs
+++ ChatBackend/obj/Debug/net8.0/ChatBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20c9f5898e5d7fe838c6820c251214c93b8ab800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fcce5a7b502aeffa1fb64c6f4cbf73c31de8335")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
